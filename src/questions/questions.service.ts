352a9f43d5892e32cb91b8f14b3b50a79bf23bbf
Pedrohrd <pedrohrd87@gmail.com>
2025-03-11 23:03:11 +0000

    feat: gerar quizzes

--- src/questions/questions.service.ts
+++ src/questions/questions.service.ts
@@ -1,39 +1,127 @@
-import { GoogleGenerativeAI } from '@google/generative-ai'
-import { Injectable } from '@nestjs/common'
-import { FilterPromptsDTO, TipoQuestao } from './DTO/filterPrompts.DTO'
-import { QuestaoGerada } from './DTO/QuestaoGerada.DTO'
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai'
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common'
+import { PrismaService } from 'src/config/Database/Prisma.service'
+import { FilterQuizDTO } from './DTO/filterQuiz.DTO'
+import { Readable } from 'stream'
 
 @Injectable()
 export class QuestionsService {
-  async geminiService(
-    filterPrompts: FilterPromptsDTO,
-  ): Promise<QuestaoGerada[] | string> {
-    const API_GEMINI = process.env.API_GEMINI || ''
+  private readonly modelGeminiFlash: GenerativeModel
+  constructor(
+    private readonly genAI: GoogleGenerativeAI,
+    private readonly prismaService: PrismaService,
+  ) {
+    const API_KEY = process.env.API_GEMINI || ''
+    this.genAI.apiKey = API_KEY
+    this.modelGeminiFlash = this.genAI.getGenerativeModel({
+      model: 'gemini-1.5-flash',
+    })
+  }
 
-    const genAI = new GoogleGenerativeAI(API_GEMINI)
-    const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
+  async gerarQuiz(filterQuizDTO: FilterQuizDTO, user: any) {
+    console.log(user)
 
-    const {
-      quantidade,
-      tipoDePergunta,
+    let {
+      titulo,
+      tipoPergunta,
       tema,
-      nivelDeDificuldade,
-      contextoEducacional,
-    } = filterPrompts
+      status,
+      quantidade,
+      nivelDificuldade,
+      professorId,
+    } = filterQuizDTO
 
+    professorId = user.id
+
+    try {
+      const quizExisting = await this.prismaService.quiz.findFirst({
+        where: {
+          titulo,
+          professorId,
+        },
+      })
+
+      if (quizExisting) {
+        throw new UnauthorizedException('Quiz já existe')
+      }
+
+      const createdQuiz = await this.prismaService.quiz.create({
+        data: {
+          titulo,
+          tipoPergunta,
+          tema,
+          status,
+          quantidade,
+          nivelDificuldade,
+          professorId,
+          link: '',
+        },
+      })
+
+      if (!createdQuiz) {
+        throw new UnauthorizedException('Erro ao criar quiz')
+      }
+
+      return createdQuiz
+    } catch (error) {
+      console.error('Erro ao gerar e salvar quiz:', error)
+      return 'Ocorreu um erro ao gerar e armazenar o quiz.'
+    }
+  }
+
+  async findAllQuiz(user: any) {
+    const { id } = user
+
+    try {
+      const quizzes = await this.prismaService.quiz.findMany({
+        where: {
+          professorId: id,
+        },
+      })
+
+      if (!quizzes) {
+        throw new UnauthorizedException('Nenhum quiz encontrado')
+      }
+
+      return quizzes
+    } catch (error) {
+      console.error('Erro ao buscar quizzes:', error)
+      return 'Ocorreu um erro ao buscar os quizzes.'
+    }
+  }
+
+  async gerarQuestoes(quizId: string, user: any): Promise<string> {
+    const quiz = await this.prismaService.quiz.findFirst({
+      where: {
+        professorId: user.id,
+        id: {
+          equals: quizId,
+        },
+      },
+    })
+
+    if (!quiz) {
+      throw new NotFoundException(
+        'Quiz não encontrado ou não pertence ao usuário.',
+      )
+    }
+
+    // Criando o prompt para geração de questões
     const prompt = `
-      Gere ${quantidade} questões do tipo ${tipoDePergunta === TipoQuestao.MULTIPLA_ESCOLHA ? 'Múltipla Escolha' : 'Verdadeiro ou Falso'} 
-      sobre o tema ${tema}. O nível de dificuldade deve ser ${nivelDeDificuldade}.
+      Gere ${quiz.quantidade} questões do tipo ${quiz.tipoPergunta === 'multipla_escolha' ? 'Múltipla Escolha' : 'Verdadeiro ou Falso'}
+      sobre o tema "${quiz.tema}". O nível de dificuldade deve ser "${quiz.nivelDificuldade}".
   
-      Se possível, siga o contexto educacional: ${contextoEducacional || 'Geral'}.
-  
-      Formato esperado de resposta em JSON (um array de objetos):
+      Formato esperado da resposta em JSON:
       [
         {
-          "pergunta": "Texto da pergunta gerada",
-          "tipo": "${tipoDePergunta}",
+          "pergunta": "Texto da questão gerada",
+          "tipo": "${quiz.tipoPergunta}",
           "alternativas": ${
-            tipoDePergunta === TipoQuestao.MULTIPLA_ESCOLHA
+            quiz.tipoPergunta === 'multipla_escolha'
               ? `[ { "letra": "A", "texto": "Alternativa A" },
                  { "letra": "B", "texto": "Alternativa B" },
                  { "letra": "C", "texto": "Alternativa C" },
@@ -41,14 +129,13 @@ export class QuestionsService {
                   { "letra": "E", "texto": "Alternativa E" } ]`
               : `null`
           },
-          "respostaCorreta": ${tipoDePergunta === TipoQuestao.MULTIPLA_ESCOLHA ? '"C"' : '"Verdadeiro"'},
+          "respostaCorreta": ${quiz.tipoPergunta === 'multipla_escolha' ? '"C"' : '"Verdadeiro"'},
           "explicacao": "Explicação detalhada sobre a resposta correta."
         }
       ]
     `
-
     try {
-      const result = await model.generateContent(prompt)
+      const result = await this.modelGeminiFlash.generateContent(prompt)
 
       if (!result || !result.response) {
         throw new Error('Resposta vazia da API Gemini')
@@ -58,12 +145,52 @@ export class QuestionsService {
         .text()
         .replace(/```json|```/g, '')
         .trim()
-      const questoes: QuestaoGerada[] = JSON.parse(responseText)
+      const questoesGeradas = JSON.parse(responseText)
 
-      return questoes
+      if (!Array.isArray(questoesGeradas) || questoesGeradas.length === 0) {
+        throw new Error('Formato inválido da resposta da IA')
+      }
+
+      const questoesStream = Readable.from(questoesGeradas)
+
+      questoesStream.on('data', async (questao) => {
+        try {
+          const question = await this.prismaService.question.create({
+            data: {
+              quizId: quiz.id,
+              pergunta: questao.pergunta,
+              tipo: questao.tipo,
+              respostaCorreta: questao.respostaCorreta,
+              explicacao: questao.explicacao,
+            },
+          })
+
+          if (questao.alternativas && Array.isArray(questao.alternativas)) {
+            await this.prismaService.alternative.createMany({
+              data: questao.alternativas.map((alt) => ({
+                questionId: question.id,
+                letra: alt.letra,
+                texto: alt.texto,
+              })),
+            })
+          }
+
+          console.log(`Questão salva: ${question.id}`)
+        } catch (error) {
+          console.error('Erro ao salvar questão:', error)
+        }
+      })
+
+      questoesStream.on('end', () => {
+        console.log('Todas as questões foram processadas.')
+      })
+
+      return 'As questões estão sendo geradas e armazenadas no banco.'
     } catch (error) {
-      console.error('Erro ao gerar questões:', error)
-      return 'Ocorreu um erro ao gerar as questões.'
+      console.error('Erro ao gerar e salvar questões:', error)
+      throw new UnauthorizedException(
+        'Ocorreu um erro ao gerar e armazenar as questões.',
+      )
     }
   }
 }
